e7b9ce1eca53b3817039aa4ebe5cbead65072527
czelabueno <c.zelabueno@gmail.com>
2021-08-21 12:46:17 -0500

    adding first changes without consul implementation

--- src/main/java/com/perujug/StockClientCommand.java
+++ src/main/java/com/perujug/StockClientCommand.java
@@ -6,16 +6,16 @@ import picocli.CommandLine.Command;
 
 import javax.inject.Inject;
 
-@Command
+@Command(name = "stockclient", description = "...", mixinStandardHelpOptions = true)
 public class StockClientCommand implements Runnable {
     @Inject
     private StockClient stockClient;
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         PicocliRunner.run(StockClientCommand.class, args);
     }
 
-    public void run(){
+    public void run() {
         System.out.println(stockClient.getStock("ORCL"));
         System.out.println(stockClient.getStock("MSFT"));
     }
